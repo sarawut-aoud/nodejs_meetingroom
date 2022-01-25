a10b2e7d2c29dd93645b7a09a269117b69ab7603
sarawut-pcru <u.sarawut586@gmail.com>
2022-01-25 11:09:00 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+// app.js
+const express = require("express")
+const path = require('path')
+
+const app = express();
+
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, 'views/index.html'))
+});
+
+app.listen(4500, () => {
+  console.log('Listening on port ' + 4500);
+});
+
