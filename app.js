670d66ffcbf156c8c014b14bb441603ff9728ec0
sarawut-pcru <u.sarawut586@gmail.com>
2022-01-27 16:05:48 +0700

    json

--- app.js
+++ app.js
@@ -9,10 +9,15 @@ app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/
 app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
 app.use('/style', express.static(path.join(__dirname, 'dist/styles')));
 app.use('/img', express.static(path.join(__dirname, 'dist/images')));
+// app.use('/link',express.static(path.join(__dirname,'views')))
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, 'views/index.html'))
 });
+app.get("/show",(req, res)=>{
+    res.sendFile(path.join(__dirname,'views/show.html'))
+})
+    
 
 app.listen(4500, () => {
     console.log('Listening on port ' + 4500);
