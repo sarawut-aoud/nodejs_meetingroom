e9875391fe0cd1632ca8b106353daf1f3d61f0fe
sarawut-pcru <u.sarawut586@gmail.com>
2022-01-27 13:51:33 +0700

    update css

--- app.js
+++ app.js
@@ -7,12 +7,13 @@ const app = express();
 app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
+app.use('/style', express.static(path.join(__dirname, 'dist/styles')));
+app.use('/img', express.static(path.join(__dirname, 'dist/images')));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, 'views/index.html'))
+    res.sendFile(path.join(__dirname, 'views/index.html'))
 });
 
 app.listen(4500, () => {
-  console.log('Listening on port ' + 4500);
+    console.log('Listening on port ' + 4500);
 });
-
