26fc79364986c14dcf685eeee42d5ba4f1477190
sarawut-pcru <sarawut.aou@pcru.ac.th>
2022-02-02 21:57:07 +0700

    plugins

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const depart = require("./models/department");
 const room = require("./models/product");
 const style = require("./models/roomstyle");
 
-
 const MONGODB_URI =
   process.env.MONGODB_URI || "mongodb://localhost:27017/db_demo";
 const PORT = process.env.PORT || 4500;
@@ -17,6 +16,46 @@ mongoose.connection.on("error", (err) => {
   console.error("MongoDB error", err);
 });
 
+app.use(
+  "/css",
+  express.static(path.join(__dirname, "node_modules/bootstrap/dist/css")),
+  express.static(path.join(__dirname, "plugins/fontawesome-free/css")),
+  express.static(path.join(__dirname, "plugins/select2/css")),
+  express.static(path.join(__dirname, "plugins/select2-bootstrap4-theme")),
+  express.static(path.join(__dirname, "plugins/tempusdominus-bootstrap-4/css")),
+  express.static(path.join(__dirname, "plugins/overlayScrollbars/css")),
+  express.static(path.join(__dirname, "plugins/icheck-bootstrap")),
+  express.static(path.join(__dirname, "plugins/daterangepicker")),
+  express.static(path.join(__dirname, "plugins/summernote")),
+  express.static(path.join(__dirname, "plugins/sweetalert2-theme-bootstrap-4")),
+  express.static(path.join(__dirname, "plugins/toastr")),
+);
+app.use(
+  "/js",
+  express.static(path.join(__dirname, "node_modules/jquery/dist")),
+  express.static(path.join(__dirname, "node_modules/bootstrap/dist/js")),
+  express.static(path.join(__dirname, "plugins/jquery-ui")),
+  express.static(path.join(__dirname, "plugins/bootstrap/js")),
+  express.static(path.join(__dirname, "plugins/select2/js")),
+  express.static(path.join(__dirname, "plugins/moment")),
+  express.static(path.join(__dirname, "plugins/daterangepicker")),
+  express.static(path.join(__dirname, "plugins/tempusdominus-bootstrap-4/js")),
+  express.static(path.join(__dirname, "plugins/summernote")),
+  express.static(path.join(__dirname, "plugins/overlayScrollbars/js")),
+  express.static(path.join(__dirname, "plugins/sweetalert2")),
+  express.static(path.join(__dirname, "plugins/toastr")),
+
+);
+app.use(
+  "/style",
+  express.static(path.join(__dirname, "public/styles")),
+  express.static(path.join(__dirname, "public/javascript")),
+  express.static(path.join(__dirname, "public/images"))
+);
+
+
+
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -25,63 +64,68 @@ app.get("/", (req, res) => {
 app.get("/user", (req, res) => {
   res.sendFile(path.join(__dirname, "views/users.html"));
 });
-app.get("/room", async (req, res) => {
-  const products = await room.find({});
-  res.json(products);
-});
-app.get("/style", async (req, res) => {
-  const products = await style.find({});
-  res.json(products);
-});
-app.get("/depart", async (req, res) => {
-  const products = await depart.find({});
-  res.json(products);
+app.get("/manage", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/manager.html"));
 });
 
-app.get("/api/:id", async (req, res) => {
-  const { id } = req.params;
+// ! database
+// app.get("/room", async (req, res) => {
+//   const products = await room.find({});
+//   res.json(products);
+// });
+// app.get("/style", async (req, res) => {
+//   const products = await style.find({});
+//   res.json(products);
+// });
+// app.get("/depart", async (req, res) => {
+//   const products = await depart.find({});
+//   res.json(products);
+// });
 
-  try {
-    const product = await Product.findById(id);
-    res.json(product);
-  } catch (error) {
-    res.status(400).json(error);
-  }
-});
+// app.get("/api/:id", async (req, res) => {
+//   const { id } = req.params;
 
-app.post("/api", async (req, res) => {
-  const payload = req.body;
-  try {
-    const product = new Product(payload);
-    await product.save();
-    res.status(201).end();
-  } catch (error) {
-    res.status(400).json(error);
-  }
-});
+//   try {
+//     const product = await Product.findById(id);
+//     res.json(product);
+//   } catch (error) {
+//     res.status(400).json(error);
+//   }
+// });
 
-app.put("/api/:id", async (req, res) => {
-  const payload = req.body;
-  const { id } = req.params;
+// app.post("/api", async (req, res) => {
+//   const payload = req.body;
+//   try {
+//     const product = new Product(payload);
+//     await product.save();
+//     res.status(201).end();
+//   } catch (error) {
+//     res.status(400).json(error);
+//   }
+// });
 
-  try {
-    const product = await Product.findByIdAndUpdate(id, { $set: payload });
-    res.json(product);
-  } catch (error) {
-    res.status(400).json(error);
-  }
-});
+// app.put("/api/:id", async (req, res) => {
+//   const payload = req.body;
+//   const { id } = req.params;
 
-app.delete("/api/:id", async (req, res) => {
-  const { id } = req.params;
+//   try {
+//     const product = await Product.findByIdAndUpdate(id, { $set: payload });
+//     res.json(product);
+//   } catch (error) {
+//     res.status(400).json(error);
+//   }
+// });
 
-  try {
-    await Product.findByIdAndDelete(id);
-    res.status(204).end();
-  } catch (error) {
-    res.status(400).json(error);
-  }
-});
+// app.delete("/api/:id", async (req, res) => {
+//   const { id } = req.params;
+
+//   try {
+//     await Product.findByIdAndDelete(id);
+//     res.status(204).end();
+//   } catch (error) {
+//     res.status(400).json(error);
+//   }
+// });
 
 app.listen(PORT, () => {
   console.log(`Application is running on port ${PORT}`);
