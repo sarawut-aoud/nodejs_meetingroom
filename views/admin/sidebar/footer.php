636d98a042e92d2032146d22f0a65699af9e74fa
sarawut-pcru <u.sarawut586@gmail.com>
2022-02-14 14:35:46 +0700

    update footer

--- views/admin/sidebar/footer.php
+++ views/admin/sidebar/footer.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
     <div class="float-right d-none d-sm-block">
-       <b> Version 1.0.0
+       <b> Version 1.0.0 </b>
     </div>
     <strong>Copyright &copy; 2022 ศูนย์คอมพิวเตอร์ โรงพยาบาลเพชรบูรณ์ สงวนลิขสิทธิ์ </strong> All rights reserved.
 </footer>
