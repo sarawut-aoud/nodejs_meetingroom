83f5ef7ae93f6992cac7e45726ef570950744262
sarawut-pcru <sarawut.aou@pcru.ac.th>
2022-02-18 22:11:18 +0700

    commit

--- views/admin/room_reserve.php
+++ views/admin/room_reserve.php
@@ -203,14 +203,14 @@ if ($_SESSION['mt_lv_id'] == 1) {
                 url: path + "/event",
                 success: function(data) {
                     var table = '<table id="tb_RoomAll" with="100%" class="table table-hover text-nowrap">' +
-                        '<thead><tr><th>ลำดับ</th><th>สถานที่ประชุม</th><th>หัวข้อเรื่องประชุม</th><th>ตั้งแต่เวลา</th><th>ถึงเวลา</th><th>สถานะ</th><th></th></thead></tr>';
+                        '<thead><tr><th>ลำดับ</th><th>สถานที่ประชุม</th><th>หัวข้อเรื่องประชุม</th><th>ตั้งแต่เวลา</th><th>ถึงเวลา</th><th>สถานะ</th><th>รายละเอียด</th><th></th></thead></tr>';
                     $.each(data, function(idx, cell) {
                         if (cell.ev_status == 5) {
                             var bage3 = '<span class="badge rounded-pill bg-danger">ยกเลิก</span>';
                         } else if (cell.ev_status == 4) {
                             var bage3 = '<span class="badge rounded-pill bg-secondary">ไม่อนุมัติ</span>';
-                            var btn = '<a id="' + cell.ev_id + '" class="btn btn-info "><i class="fa-solid fa-eye"></i></a>' +
-                                ' <a id="' + cell.ev_id + '" class="btn btn-warning btnRoomEdit"><i class="fas fa-edit"></i></a>' +
+                            var detail = '<a id="' + cell.ev_id + '" class="btn btn-info "><i class="fa-solid fa-eye"></i></a>'
+                            var btn = ' <a id="' + cell.ev_id + '" class="btn btn-warning btnRoomEdit"><i class="fas fa-edit"></i></a>' +
                                 ' <a id="' + cell.ev_id + '" class="btn btn-danger btnRoomDels"><i class="fas fa-trash-alt"></i></a>'
                         } else if (cell.ev_status == 3) {
                             var bage3 = '<span class="badge rounded-pill bg-success">อนุมัติ</span>';
@@ -220,13 +220,13 @@ if ($_SESSION['mt_lv_id'] == 1) {
                             var bage3 = '<span class="badge rounded-pill bg-warning">ไม่อนุมัติจากหัวหน้า</span>';
                         } else if (cell.ev_status == 1) {
                             var bage3 = '<span class="badge rounded-pill bg-warning">รอการอนุมัติ</span>';
-                            var btn = '<a id="' + cell.ev_id + '" class="btn btn-info "><i class="fa-solid fa-eye"></i></a>' +
-                                ' <a id="' + cell.ev_id + '" class="btn btn-warning btnRoomEdit"><i class="fas fa-edit"></i></a>' +
+                            var deatil = '<a id="' + cell.ev_id + '" class="btn btn-info "><i class="fa-solid fa-eye"></i></a>'
+                            var btn = ' <a id="' + cell.ev_id + '" class="btn btn-warning btnRoomEdit"><i class="fas fa-edit"></i></a>' +
                                 ' <a id="' + cell.ev_id + '" class="btn btn-danger btnRoomDels"><i class="fas fa-trash-alt"></i></a>'
                         } else if (cell.ev_status == 0) {
                             var bage3 = '<span class="badge rounded-pill  bg-warning">รอการอนุมัติจากหัวหน้า</span>';
-                            var btn = '<a id="' + cell.ev_id + '" class="btn btn-info "><i class="fa-solid fa-eye"></i></a>' +
-                                ' <a id="' + cell.ev_id + '" class="btn btn-warning btnRoomEdit"><i class="fas fa-edit"></i></a>' +
+                            var deatil = '<a id="' + cell.ev_id + '" class="btn btn-info "><i class="fa-solid fa-eye"></i></a>'
+                            var btn = ' <a id="' + cell.ev_id + '" class="btn btn-warning btnRoomEdit"><i class="fas fa-edit"></i></a>' +
                                 ' <a id="' + cell.ev_id + '" class="btn btn-danger btnRoomDels"><i class="fas fa-trash-alt"></i></a>'
                         }
 
@@ -238,6 +238,7 @@ if ($_SESSION['mt_lv_id'] == 1) {
                         table += ('<td>' + cell.ev_startdate + ' เวลา ' + cell.ev_starttime + '</td>');
                         table += ('<td>' + cell.ev_enddate + ' เวลา ' + cell.ev_endtime + '</td>');
                         table += ('<td align="center">' + bage3 + '</td>');
+                        table += ('<td align="center" width="20%">' + deatil + '</td>');
                         table += ('<td align="center" width="20%">' + btn + '</td>');
                         table += ('</tr>');
                     });
