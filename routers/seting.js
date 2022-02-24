20eb56595060f0ecc3154f622dbfd6074fbb15e8
sarawut-pcru <sarawut.aou@pcru.ac.th>
2022-02-24 23:24:05 +0700

    device seting

--- routers/seting.js
+++ routers/seting.js
@@ -21,18 +21,13 @@ router.get("/", async (req, res) => {
   } else {
     con.query(
       "SELECT ev.ev_id , ev.ev_createdate, ev.ev_title ,ev.ev_startdate,ev.ev_enddate, ev.ev_starttime ,ev.ev_endtime, ev.ev_status ,ev.ev_people," +
-        "ac.ev_id , ac.to_id,"+
-        "ro.ro_name , tool.to_name, st.st_name"+
-        "users.id, users.firstname,users.lastname, users.position," +
-        "dept.de_id, dept.de_name, dept.de_phone"+
-        "FROM tbl_acces AS ac " +
-        "INNER JOIN tbl_event AS ev "+
-        "INNER JOIN tbl_user AS users ON (ev.id = users.id ) " +
-        "INNER JOIN tbl_department AS dept ON (users.de_id = dept.de_id) " +
-        "INNER JOIN tbl_rooms AS ro ON (ev.ro_id = ro.ro_id) " +
-        "INNER JOIN tbl_style AS st ON (ev.st_id = st.st_id) " +
-        "INNER JOIN tbl_tools AS tool tool (tool.to_id = ac.to_id) " +
-        "WHERE ev.ev_status = '3' AND tool.de_id = ? AND tool.to_id = ac.to_id " +
+        "ro.ro_id,ro.ro_name , users.id " +
+        "FROM tbl_event AS ev " +
+        "INNER JOIN tbl_rooms AS ro " +
+        "ON (ev.ro_id = ro.ro_id) " +
+        "INNER JOIN tbl_user AS users " +
+        "ON (ev.id = users.id ) " +
+        "WHERE users.id = ? " +
         "GROUP BY ev.event_id",
       [id],
       (error, results, fields) => {
@@ -43,19 +38,19 @@ router.get("/", async (req, res) => {
           var ev_status = results[i].ev_status;
           if (ev_id) {
             con.query(
-              "SELECT * FROM tbl_setdevice WHERE ev_id = ? AND id = ? AND dv_status = ?",
+              "SELECT * FROM tbl_seting WHERE ev_id = ? AND id = ? AND set_status = ?",
               [ev_id, id, ev_status],
               (error, total, fields) => {
                 if (error) throw error;
                 for (var x = 0; x < total.length; x++) {
                   if (total == 0) {
                     con.query(
-                      "DELETE FROM tbl_setdevice WHERE ev_id = ?",
+                      "DELETE FROM tbl_seting WHERE ev_id = ?",
                       [ev_id],
                       (error, results, fields) => {
                         if (error) throw error;
                         con.query(
-                          "INSERT INTO tbl_setdevice(id,ev_id,dv_status) " +
+                          "INSERT INTO tbl_seting(id,ev_id,set_status) " +
                             "VALUES(?,?,?)",
                           [id, ev_id, ev_status],
                           (error, results, fields) => {
@@ -65,12 +60,12 @@ router.get("/", async (req, res) => {
                         );
                       }
                     );
-                  }//if total
-                }//for x
+                  }
+                }
               }
             );
           }
-        }//for i 
+        }
       }
     );
   }
